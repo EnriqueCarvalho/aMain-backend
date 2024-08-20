a20287f85391e839eb813cead056cede3e7768a8
enrique.carvalho@avmb-asten.com.br <enrique.carvalho>
2024-08-20 13:31:31 -0300

    feat: inicio do projeto

---
+++ src/main/java/br/com/amain/backend/demo/AmainBackApplication.java
@@ -0,0 +1,13 @@
+package br.com.amain.backend.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmainBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmainBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/amain/backend/demo/AmainBackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.amain.backend.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmainBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
